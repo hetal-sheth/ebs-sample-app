3c3f0ed4ee2926cc31adbaf4e4d1a17853a27ccf
hetal_s <hetal@creamsonservices.com>
2020-07-29 15:13:59 +0530

    import removed

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from activity import student_favorite
+# from activity import student_favorite
 
 
 application = Flask(__name__)
 
-application.register_blueprint(student_favorite, url_prefix='/favoriteContent')
+# application.register_blueprint(student_favorite, url_prefix='/favoriteContent')
 
 @application.route('/')
 def index():
